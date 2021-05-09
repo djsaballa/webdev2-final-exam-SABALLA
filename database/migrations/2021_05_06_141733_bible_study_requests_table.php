f6a409765257739f726180138d73c8ee6754936c
djsaballa <71679357+djsaballa@users.noreply.github.com>
2021-05-09 13:12:41 +0800

    Update 2021_05_06_141733_bible_study_requests_table.php

--- database/migrations/2021_05_06_141733_bible_study_requests_table.php
+++ database/migrations/2021_05_06_141733_bible_study_requests_table.php
@@ -15,14 +15,14 @@ class BibleStudyRequestsTable extends Migration
     {
         Schema::create('bs_requests', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 50)->nullable()->default(null);
-            $table->string('email')->nullable()->default(null);
-            $table->bigInteger('contact_number')->nullable()->default(null);
-            $table->date('birthdate')->nullable()->default(null);
-            $table->string('religion')->nullable()->default(null);
-            $table->date('bs_date')->nullable()->default(null);
-            $table->time('bs_time')->nullable()->default(null);
-            $table->string('bs_address')->nullable()->default(null);
+            $table->string('name', 50);
+            $table->string('email');
+            $table->bigInteger('contact_number');
+            $table->date('birthdate');
+            $table->string('religion');
+            $table->date('bs_date');
+            $table->time('bs_time');
+            $table->string('bs_address');
             $table->timestamps();
         });
     }
